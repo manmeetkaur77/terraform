eefd30ec275864115b7ede4bed888179b4f6e667
manmeetkaur77 <155020076+manmeetkaur77@users.noreply.github.com>
2026-03-27 18:20:53 +0530

    chore: add modules/alb/outputs.tf

---
+++ modules/alb/outputs.tf
@@ -0,0 +1,29 @@
+output "alb_id" {
+  description = "ID of the Application Load Balancer"
+  value       = aws_lb.main.id
+}
+
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.main.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_zone_id" {
+  description = "Zone ID of the Application Load Balancer"
+  value       = aws_lb.main.zone_id
+}
+
+output "https_listener_arn" {
+  description = "ARN of the HTTPS listener"
+  value       = aws_lb_listener.https.arn
+}
+
+output "http_listener_arn" {
+  description = "ARN of the HTTP listener"
+  value       = aws_lb_listener.http.arn
+}
